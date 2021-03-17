8b12d04b84eb0e23b7ccd2fbe01d6a31a418e9ea
Amit Kumar <amit@callcomm.com>
2021-03-17 12:54:42 +0530

    service

--- trainsoft-ui/src/index.js
+++ trainsoft-ui/src/index.js
@@ -6,6 +6,7 @@ import { AppProvider } from './Store/AppContext';
 import AxiosService from './Services/axios.service';
 import GLOBELCONSTANT from './Constant/GlobleConstant';
 import { ReactBootstrapAlert } from "rct-bs-alert";
+import * as serviceWorker from './serviceWorker';
 // import { ZoomMtg } from "@zoomus/websdk";
 // For Local module default:
 // ZoomMtg.setZoomJSLib('node_modules/@zoomus/websdk/dist/lib', '/av');
@@ -26,4 +27,9 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
