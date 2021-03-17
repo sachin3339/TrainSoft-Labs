fbe02fc87e94873eb18d959c29c96ed6ae56688d
Amit Kumar <amit@callcomm.com>
2021-03-17 11:39:36 +0530

    api changes

--- trainsoft-ui/src/index.js
+++ trainsoft-ui/src/index.js
@@ -6,6 +6,14 @@ import { AppProvider } from './Store/AppContext';
 import AxiosService from './Services/axios.service';
 import GLOBELCONSTANT from './Constant/GlobleConstant';
 import { ReactBootstrapAlert } from "rct-bs-alert";
+// import { ZoomMtg } from "@zoomus/websdk";
+// For Local module default:
+// ZoomMtg.setZoomJSLib('node_modules/@zoomus/websdk/dist/lib', '/av');
+// ZoomMtg.preLoadWasm();
+// ZoomMtg.prepareJssdk();
+// const zoomMeeting = document.getElementById("zmmtg-root")
+// ZoomMtg.init()
+
 AxiosService.init(GLOBELCONSTANT.BASE_URL);
 
 ReactDOM.render(
