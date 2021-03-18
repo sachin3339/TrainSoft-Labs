cff1a5429f479a14ec3057cc41f9a97f8f7c7e19
Amit Kumar <amit@callcomm.com>
2021-03-18 12:28:54 +0530

    conflict resolved

--- trainsoft-ui/src/index.js
+++ trainsoft-ui/src/index.js
@@ -5,15 +5,19 @@ import App from './App';
 import { AppProvider } from './Store/AppContext';
 import { ReactBootstrapAlert } from "rct-bs-alert";
 import * as serviceWorker from './serviceWorker';
+import { HelmetProvider } from 'react-helmet-async';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <HelmetProvider>
     <ReactBootstrapAlert>
           <AppProvider>
         <App />
     </AppProvider>
     </ReactBootstrapAlert>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
