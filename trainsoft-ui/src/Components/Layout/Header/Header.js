642e5fe6a8730295414eb1e95e691eca9046a768
gouthaml21 <goutham@akalmatechnologies.com>
2021-03-24 14:52:46 +0530

    UI Update

--- trainsoft-ui/src/Components/Layout/Header/Header.js
+++ trainsoft-ui/src/Components/Layout/Header/Header.js
@@ -26,48 +26,50 @@ const Header = ({location}) => {
     return (<>
     { location.state.title !== "COMPILER" &&
     <div className="header">
-            <div className="title-lg mb-0">
-                {location.state && location.state.title}
+            <div className="page-title">
+                <div className="title-lg mb-0">
+                    {location.state && location.state.title}
+                </div>
+                <div class="breadcrumb-wrap"><a href="#">Summary</a><a href="#">Level1</a><a href="#">Level2</a></div>
             </div>
-           
             <div className="aic">
-            <Dropdown className="notification">
+                <Dropdown className="notification">
                     <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
                        <div className="mx-3">{ICN_NOTIFICATION}</div>
                     </Dropdown.Toggle>
-                <Dropdown.Menu as="div" align="left">
-                    <div className="my-2">
-                        <div className="title-sm">Lorem Ipsum</div>
-                        <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
-                    </div>
-                    <div className="my-2">
-                        <div className="title-sm">Lorem Ipsum</div>
-                        <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
-                    </div>
-                    <div className="my-2">
-                        <div className="title-sm">Lorem Ipsum</div>
-                        <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
-                    </div>
-                    <div className="my-2">
-                        <div className="title-sm">Lorem Ipsum</div>
-                        <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
-                    </div>
-                    <div className="my-2">
-                        <div className="title-sm">Lorem Ipsum</div>
-                        <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
-                    </div>
-                        <div className="text-md text-center pointer">View More</div>  
-                </Dropdown.Menu>
-            </Dropdown>
+                    <Dropdown.Menu as="div" align="left">
+                        <div className="my-2">
+                            <div className="title-sm">Lorem Ipsum</div>
+                            <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
+                        </div>
+                        <div className="my-2">
+                            <div className="title-sm">Lorem Ipsum</div>
+                            <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
+                        </div>
+                        <div className="my-2">
+                            <div className="title-sm">Lorem Ipsum</div>
+                            <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
+                        </div>
+                        <div className="my-2">
+                            <div className="title-sm">Lorem Ipsum</div>
+                            <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
+                        </div>
+                        <div className="my-2">
+                            <div className="title-sm">Lorem Ipsum</div>
+                            <div className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisci</div>
+                        </div>
+                            <div className="text-md text-center pointer">View More</div>  
+                    </Dropdown.Menu>
+                </Dropdown>
             
-            <Dropdown className="">
-                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-                         <div><ProfileImg name={getUserName(user.name)} size="md"/></div>
-                    </Dropdown.Toggle>
-                <Dropdown.Menu as="div" align="left">
-                  <Dropdown.Item onClick={()=>navigate('/')}>Logout</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
+                <Dropdown className="">
+                        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                            <div><ProfileImg name={getUserName(user.name)} size="md"/></div>
+                        </Dropdown.Toggle>
+                    <Dropdown.Menu as="div" align="left">
+                    <Dropdown.Item onClick={()=>navigate('/')}>Logout</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
             
     </div>}</>)
