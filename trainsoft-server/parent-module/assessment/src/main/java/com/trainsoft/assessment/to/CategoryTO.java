ec92b94a0f3337b00ac9c2d3ea48162d4944c63c
Kalyan <kalyan@akalmatechnologies.com>
2021-04-22 02:01:06 +0530

    Changes of Category

--- trainsoft-server/parent-module/assessment/src/main/java/com/trainsoft/assessment/to/CategoryTO.java
+++ trainsoft-server/parent-module/assessment/src/main/java/com/trainsoft/assessment/to/CategoryTO.java
@@ -1,13 +1,16 @@
 package com.trainsoft.assessment.to;
-
-import com.trainsoft.assessment.enums.Status;
+import com.trainsoft.assessment.value.AssessmentEnum;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Setter @Getter
 public class CategoryTO extends BaseTO{
     private static final long serialVersionUID = 5139359445642795894L;
 
     private String name;
-    private Status status;
+    private AssessmentEnum.Status status;
+    private List<TagTo> tags;
+
 }
