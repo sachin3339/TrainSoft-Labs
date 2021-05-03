e9c75a0b696e7046914db1cc4d1f81dddb9a7798
Kalyan <kalyan@akalmatechnologies.com>
2021-05-03 13:40:35 +0530

    [KL_ASS-SCRIPT]: Changes to handle DELETED status of Topic , Tag and Assessment Repository

--- trainsoft-server/parent-module/assessment/src/main/java/com/trainsoft/assessment/repository/IAssessmentRepository.java
+++ trainsoft-server/parent-module/assessment/src/main/java/com/trainsoft/assessment/repository/IAssessmentRepository.java
@@ -12,11 +12,15 @@ import java.util.List;
 
 public interface IAssessmentRepository extends JpaRepository<Assessment,Integer>
 {
+    @Query("FROM Assessment  as assess WHERE assess.status <> 'DELETED' AND assess.sid=:assessmentSid")
     Assessment findAssessmentBySid(byte[] assessmentSid);
+    @Query("FROM Assessment as assess WHERE assess.status <> 'DELETED' AND assess.sid=:sid")
     Assessment findBySid(byte [] sid);
+    @Query("FROM Assessment as assess WHERE assess.status <> 'DELETED' AND assess.title=:title")
     Assessment findAssessmentByTitle(String title);
-    @Query(value = "select * from quiz_set where tag=:id and difficulty=:df",nativeQuery = true)
+    @Query(value = "select * from quiz_set where tag=:id and difficulty=:df and status<>'DELETED'",nativeQuery = true)
     List<Assessment> findByTagAndDifficulty(@Param("id") Integer tagId,@Param("df") String difficulty);
+    @Query("FROM Assessment  as assess WHERE assess.status <> 'DELETED' AND assess.topicId=:topic")
     List<Assessment> findAssessmentByTopicId(Topic topic, Pageable pageable);
 
 }
