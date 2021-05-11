49778d60586d0dbcf17804a3219c1048466c1f0b
Rahul Singh <rahul@callcomm.com>
2021-05-11 17:18:45 +0530

    time up modal implemented

---
+++ trainsoft-ui/src/Components/Screens/Assessment/AssessmentBody/QuitModal.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BsModal } from "../../../Common/BsUtils";
+import { navigate } from "../../../Common/Router";
+import Submit from "../common/SubmitButton";
+import styles from "./AssessmentBody.module.css";
+
+const QuitModal = ({show, setShow}) => {
+    return <BsModal {...{ show, setShow, headerTitle: "Are you sure?", size: "md" }}>
+    <div>
+      <div className="column f14 mb20">
+        <span>You are about to quit the assessment.</span>
+        <span>If you quit, all the progress will be unsaved and you have to take the assessment</span>
+        <span> From the beginning again!</span>
+      </div>
+      <div className="jce py20 mt20">
+        <Submit onClick={() => setShow(false)} style={{ backgroundColor: "#CECECE", color: "#333333", marginRight: "15px" }}>
+          Cancel
+        </Submit>
+        <div className={styles.quitButtonModal} onClick={() => navigate("/")}>Quit</div>
+      </div>
+    </div>
+  </BsModal>;
+}
+ 
+export default QuitModal;
