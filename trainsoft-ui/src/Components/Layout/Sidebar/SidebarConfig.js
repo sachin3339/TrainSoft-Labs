10fe9bc71beca903e2979d8936c79f1ef0629bde
Amit Kumar <amit@callcomm.com>
2021-05-18 14:09:01 +0530

    dashbord api integration

--- trainsoft-ui/src/Components/Layout/Sidebar/SidebarConfig.js
+++ trainsoft-ui/src/Components/Layout/Sidebar/SidebarConfig.js
@@ -13,7 +13,7 @@ export const AdminConfig = [
         title: "Dashboard",
         pathname: "assessment",
         disabled: true,
-        role: [GLOBELCONSTANT.ROLE.ASSESS_USER,GLOBELCONSTANT.ROLE.LEARNER]
+        role: [GLOBELCONSTANT.ROLE.ASSESS_USER]
     },
     
     {
@@ -21,14 +21,14 @@ export const AdminConfig = [
         title: "Catalogue",
         pathname: "catalogue",
         disabled: true,
-        role: [GLOBELCONSTANT.ROLE.ASSESS_USER,GLOBELCONSTANT.ROLE.LEARNER]
+        role: [GLOBELCONSTANT.ROLE.ASSESS_USER]
     },
     {
         icon: ICN_MYASSESSMENT,
         title: "My Assessment",
         pathname: "myAssessment",
         disabled: true,
-        role: [GLOBELCONSTANT.ROLE.ASSESS_USER,GLOBELCONSTANT.ROLE.LEARNER]
+        role: [GLOBELCONSTANT.ROLE.ASSESS_USER]
     },
     {
         icon: ICN_DASHBOARD,
